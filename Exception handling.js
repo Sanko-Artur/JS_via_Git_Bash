e1234ead51a98019fc814e13df15cbdfc99be8db
Artur Sanko <sanartyrv@gmail.com>
2021-12-15 01:20:50 +0300

    Corrected

--- Exception handling.js
+++ Exception handling.js
@@ -1,6 +1,6 @@
 /*
 В try catch конструкцию завернуть код: console.log(a), let a = 3. И вывести ошибку – ‘let перед использованием
-нужно объявить’. При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'
+нужно объявить’.
  */
 
 try {
@@ -8,8 +8,23 @@ try {
     let a = 3;
     console.log(a);
 
+
+} catch (err) {
+
+    if (err instanceof ReferenceError) {
+        console.log('let перед использованием нужно объявить');
+    }
+
+}
+
+/*
+При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'.
+*/
+
+try {
+
+    let a = 3;
     let result = a / 0;
-    let ;
 
     if (result === Infinity) {
         throw new SyntaxError('на ноль делить нельзя');
@@ -17,10 +32,6 @@ try {
 
 } catch (err) {
 
-    if (err instanceof ReferenceError) {
-        console.log('let перед использованием нужно объявить');
-    } else {
-        console.log(err);
-    }
+    console.log(err);
 
 }
