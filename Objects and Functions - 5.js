d31b59c24cdbb0928631dd24132893d297fae2ef
Artur Sanko <sanartyrv@gmail.com>
2021-12-15 14:57:53 +0300

    Corrected

--- Objects and Functions - 5.js
+++ Objects and Functions - 5.js
@@ -2,23 +2,33 @@
 -        Напишите функцию, которая определяет является ли число простым или нет
  */
 
-let number = 115;
+let number1 = 49;
+let number2 = 115;
+let number3 = 61;
 
 function isPrime(num) {
     if (num === 2) {
-        return true;
+        let isTrue = true;
+        console.log(isTrue);
     }
     if (num < 2) {
-        return false;
+        let isFalse = false;
+        console.log(isFalse);
     }
     for (let i = 2; i < num; i++) {
         if (num % i === 0) {
-            return false;
+            let isFalse = false;
+            console.log(isFalse);
             break;
         }
-        return true;
+        let isTrue = true;
+        console.log(isTrue);
     }
 }
 
-let result = isPrime(number);
-console.log(result);
+console.log(number1);
+isPrime(number1);
+console.log(number2);
+isPrime(number2);
+console.log(number3);
+isPrime(number3);
